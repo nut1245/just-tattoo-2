efacd187e4a9ae9454ed3ea9ded2cbe1314c683e
Nutthapon Saefong <73591319+nut1245@users.noreply.github.com>
2021-08-24 15:27:59 +0700

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import AppHeader from './components/AppHeader';
+import AppSearch from './components/AppSearch';
+import TattooItem from './components/TattooItem';
+import TattooPost from './components/TattooPost';
+import tattoos from './data/tattoos';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selectedTattoo, setSelectedTattoo] = useState(null);
+    const [searchText, setSearchText] = useState('');
+
+    function onTattooOpenClick(tattoo) {
+        setSelectedTattoo(tattoo);
+    }
+
+    function onTattooCloseClick() {
+        setSelectedTattoo(null);
+    }
+
+    const tattooItems = tattoos.filter((tattoo) => {
+        return tattoo.title.includes(searchText);
+    }).map((tattoo, index) => {
+        return <TattooItem key={index} tattoo={tattoo} onTattooClick={onTattooOpenClick} />;
+    });
+
+    let tattooPost = null;
+    if (!!selectedTattoo) {
+        tattooPost = <TattooPost tattoo={selectedTattoo} onBgClick={onTattooCloseClick} />;
+    }
+
+    return (
+        <div className="app">
+            <AppHeader />
+            <section className="app-section">
+                <div className="app-container">
+                    <AppSearch value={searchText} onValueChange={setSearchText} />
+                    <div className="app-grid">
+                        {tattooItems}
+                    </div>
+                </div>
+            </section>
+            {tattooPost}
+        </div>
+    );
 }
 
 export default App;
